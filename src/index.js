ff45511d4174187d3a2fd63a4952d2f6e2ae729f
Julian Santana <jjsantan@my.loyno.edu>
2019-11-21 16:31:44 -0500

    Fix eslint errors

--- src/index.js
+++ src/index.js
@@ -1,40 +1,41 @@
-const { Command, flags } = require('@oclif/command')
+const {Command, flags} = require('@oclif/command')
 
 class GithubFetcherCommand extends Command {
-	static args = [{
-			name: 'username',
-			required: true,
-			description: "Github Username",
-			hidden: false,
-		},
-		{
-			name: 'sort',
-			required: true,
-			description: "Sorting option",
-			hidden: false,
-			default: 'asc',
-			options: ['asc', 'desc']
-		},
-	]
-	async run() {
-		const { args } = this.parse(GithubFetcherCommand)
-		const username = args.username
-		const sort = args.sort || 'asc'
-		this.log(`Searching ${username} github, sorting in ${sort} order`)
-	}
+  static args = [{
+    name: 'username',
+    required: true,
+    description: 'Github Username',
+    hidden: false,
+  },
+  {
+    name: 'sort',
+    required: true,
+    description: 'Sorting option',
+    hidden: false,
+    default: 'asc',
+    options: ['asc', 'desc'],
+  }];
+
+  async run() {
+    const {args} = this.parse(GithubFetcherCommand)
+    const username = args.username
+    const sort = args.sort || 'asc'
+    this.log(`Searching ${username} github, sorting in ${sort} order`)
+  }
 }
 
-GithubFetcherCommand.description = `Cli tool that fetches Github repositories by username and sorts them by stargazers`
+GithubFetcherCommand.description =
+	'Cli tool that fetches Github repositories by username and sorts them by stargazers'
 
 GithubFetcherCommand.flags = {
-	// add --version flag to show CLI version
-	version: flags.version({
-		char: 'v'
-	}),
-	// add --help flag to show CLI version
-	help: flags.help({
-		char: 'h'
-	}),
+  // add --version flag to show CLI version
+  version: flags.version({
+    char: 'v',
+  }),
+  // add --help flag to show CLI version
+  help: flags.help({
+    char: 'h',
+  }),
 }
 
 module.exports = GithubFetcherCommand
